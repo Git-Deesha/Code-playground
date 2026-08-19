279a5d4256c19483901a744d2b65ecb1f5e39958
Git-Deesha <deetripathi02@gmail.com>
2026-08-19 21:56:02 +0530

    Add files via upload

---
+++ num guessing game.py
@@ -0,0 +1,16 @@
+import random
+top=int(input("enter top number"))
+num=random.randint(0,top-1)
+guess=1
+while True:
+    gnum=int(input("enter your guess"))
+    if gnum>num:
+        print("guess lower")
+        guess+=1
+    elif gnum<num:
+        print("guess higher")
+        guess+=1
+    elif gnum==num:
+        print("you guessed the number right in:", guess, end=" guesses")
+        print()
+        break
